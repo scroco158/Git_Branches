3ffbf39206fd688f84fe8407e43406c000c79b55
scroco158 <scroco@mail.ru>
2023-11-16 17:09:24 +0300

    adding new def and docstr

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def capitalizer(my_string):
+    """ делает все буквы заглавными """
     return str(my_string).upper()
+
+
+def capitalizeer_first_letter(my_string):
+    """ делает первые буквы заглавными"""
+    return str(my_string).title()
